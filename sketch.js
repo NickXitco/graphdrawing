a2b39850514924d7cb49f2c68e03c56ce7331ea9
NickXitco <nxitco1@jhu.edu>
2020-03-07 01:58:31 -0500

    Working O(n^2)  Spring-Electrical Algo

---
+++ sketch.js
@@ -0,0 +1,126 @@
+let vertices = [];
+let edges = [];
+const width = window.innerWidth;
+const height = window.innerHeight;
+const numVertices = 100;
+const tol = 0;
+let step = 0.003;
+
+const C = 3;
+const K = 10;
+
+
+class Vertex {
+    constructor() {
+        this.neighbors = [];
+    }
+
+    randPosition() {
+        this.x = (Math.random() * (width - 300)) + 150;
+        this.y = (Math.random() * (height - 300)) + 150;
+    }
+
+    addNeighbor(v) {
+        this.neighbors.push(v);
+        v.neighbors.push(this);
+    }
+
+    static angle(u, v) {
+        return Math.atan2(v.y - u.y, v.x - u.x);
+    }
+
+    static distance(u, v) {
+        return Math.sqrt(((u.x - v.x) ** 2) + ((u.y - v.y) ** 2));
+    }
+
+    static attractiveForce(u, v) {
+        let f = -1 * (Vertex.distance(u, v) ** 2) / K;
+
+        return {
+            x: Math.cos(Vertex.angle(u, v)) * f,
+            y: Math.sin(Vertex.angle(u, v)) * f
+        };
+    }
+
+    static repulsiveForce(u, v) {
+        let f = C * K * K / Vertex.distance(u, v);
+
+        return {
+            x: Math.cos(Vertex.angle(u, v)) * f,
+            y: Math.sin(Vertex.angle(u, v)) * f
+        };
+    }
+}
+
+
+function setup() {
+
+    for (let i = 0; i < numVertices; i++) {
+        vertices.push(new Vertex());
+    }
+
+    for (const v of vertices) {
+        v.randPosition();
+    }
+
+    for (let i = 0; i < vertices.length; i++) {
+        let r = Math.floor(Math.random() * (vertices.length));
+        if (r !== i && !vertices[i].neighbors.includes(vertices[r])) {
+            vertices[i].addNeighbor(vertices[r]);
+            edges.push({
+                u: vertices[i],
+                v: vertices[r]
+            });
+        }
+    }
+
+
+    createCanvas(width, height);
+    stroke(0);
+
+    strokeWeight(1);
+    smooth();
+}
+
+function draw() {
+    background(250);
+
+
+    for (const v of vertices) {
+        if (step <= 0) break;
+        let f = {
+            x: 0,
+            y: 0
+        };
+
+        for (const u of v.neighbors) {
+            const fa = Vertex.attractiveForce(u, v);
+            f.x += fa.x;
+            f.y += fa.y;
+        }
+
+        for (const j of vertices) {
+            if (j === v) continue;
+            const fr = Vertex.repulsiveForce(j, v);
+            f.x += fr.x;
+            f.y += fr.y;
+        }
+
+        v.x += step * f.x;
+        v.y += step * f.y;
+        stroke(255 - Math.min(5 * (Math.sqrt(f.x**2 + f.y**2)), 255));
+        circle(v.x, v.y, 5);
+        stroke(0);
+    }
+    step -= 0.0000001;
+
+
+    for (const v of vertices) {
+
+    }
+
+    for (const e of edges) {
+        line(e.u.x, e.u.y, e.v.x, e.v.y);
+    }
+
+}
